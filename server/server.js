065b0d7e538cbf612526c0693bb6a28cb37791d2
jhutch713 <142631513+jhutch713@users.noreply.github.com>
2024-04-10 20:09:05 +0000

    practice graph

--- server/server.js
+++ server/server.js
@@ -7,11 +7,12 @@ app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => { 
+    // res.sendFile(path.join(__dirname, '..', 'index.html'));
     res.sendFile(path.join(__dirname, '..', 'index.html'));
     //res.send('hello world');
 });
 app.get('/csv-data', (req, res) => {
-    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    const csvPath = path.join(__dirname, '..', 'data', 'crashes_catolina.csv');
     fs.readFile(csvPath, 'utf8', (err, data) => {
         if (err) { 
             console.error(err);
